b2a30c38fcaf73b4aa7c627bdd39950b6e0be760
Nolex_ <151950558+Anise-Lenaford@users.noreply.github.com>
2026-04-01 12:44:38 +0800

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -3,24 +3,26 @@
 #include <shadowhook.h>
 #include "imgui_impl_android.h"
 
-using initializeMotionEvent_ = int(*)(void*, void*, void*);
-static initializeMotionEvent_ o_initializeMotionEvent = nullptr;
-int f_initializeMotionEvent(void *inputConsumer, void *motionEvent, void *inputMessage) {
-    int result = o_initializeMotionEvent(inputConsumer, motionEvent, inputMessage);
-    auto* event = (AInputEvent*)inputConsumer;
-    ImGui_ImplAndroid_HandleInputEvent(event);
+using AInputQueue_preDispatchEvent_ = int(*)(void*, void*);
+static AInputQueue_preDispatchEvent_ o_AInputQueue_preDispatchEvent = nullptr;
+int f_AInputQueue_preDispatchEvent(void *inputQueue, void *motionEvent) {
+    int result = o_AInputQueue_preDispatchEvent(inputQueue, motionEvent);
 
-    ImGuiIO& io = ImGui::GetIO();
-    float wheel = io.MouseDelta.y ;
-    io.MouseWheel = wheel * 0.01f;
+    if (motionEvent != nullptr) {
+        ImGui_ImplAndroid_HandleInputEvent(reinterpret_cast<AInputEvent*>(motionEvent));
+
+        ImGuiIO& io = ImGui::GetIO();
+        float wheel = io.MouseDelta.y;
+        io.MouseWheel = wheel * 0.01f;
+    }
 
     return result;
 
 }
 
-void Input::Init() {
-    shadowhook_hook_sym_name("libinput.so","_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE",(void*)f_initializeMotionEvent,(void**)&o_initializeMotionEvent);
 
+void Input::Init() {
+    shadowhook_hook_sym_name("libandroid.so","AInputQueue_preDispatchEvent",(void*)f_AInputQueue_preDispatchEvent,(void**)&o_AInputQueue_preDispatchEvent);
 
 }
 
